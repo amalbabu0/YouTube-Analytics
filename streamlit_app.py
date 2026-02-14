3ba1073ba4c9be20379582f2486d956552b3ba22
Amal Babu <140903286+amalbabu0@users.noreply.github.com>
2026-02-14 20:54:11 +0530

    Restore streamlit_app.py with original content

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ max_results = st.sidebar.slider("Number of Videos", min_value=10, max_value=50, 
 # YouTube API setup
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
-DEVELOPER_KEY = st.secrets["youtube_api_key"]
+DEVELOPER_KEY = ["youtube_api_key"]
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
@@ -79,4 +79,5 @@ if st.sidebar.button("Search"):
     ax.set_xlabel("Views")
     ax.set_ylabel("Video Title")
     st.pyplot(fig)
+
 
